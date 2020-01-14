5a529ee5f992eaed54341825a6184722d5f25e57
Saralopezlovon <lopezlovonsara@gmail.com>
2020-01-14 02:26:58 +0100

    add total style of pages

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -1,15 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../stylesheets/characterDetail.scss";
 
 function CharacterDetail(props) {
   return (
-    <div className="card">
-      <Link to="/">Volver</Link>
-      <img src={props.character.image} alt={props.character.name} />
-      <h3>Status: {props.character.status}</h3>
-      <h3>Species: {props.character.species}</h3>
-      <h3>Origin: {props.character.origin.name}</h3>
-      <h3>Episodes: {props.character.episode.length}</h3>
+    <div className="detail-container">
+      <div className="link-container">
+        <Link className="link" to="/">
+          Volver
+        </Link>
+      </div>
+
+      <div className="card">
+        <img className="card-image" src={props.character.image} alt={props.character.name} />
+        <ul className="card-description-container">
+          <li className="card-description_name">{props.character.name}</li>
+          <li className="card-description">Status: {props.character.status}</li>
+          <li className="card-description">Species: {props.character.species}</li>
+          <li className="card-description">Origin: {props.character.origin.name}</li>
+          <li className="card-description">Episodes: {props.character.episode.length}</li>
+        </ul>
+      </div>
     </div>
   );
 }
