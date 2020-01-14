21862978f14ab01afcf140a1f1150108982bf07f
Saralopezlovon <lopezlovonsara@gmail.com>
2020-01-14 13:19:08 +0100

    Update docs

---
+++ docs/precache-manifest.bb02342275df41613c7500a89c87cb83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24cd28860728607118975be605aef9b5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0121c617e01b805d5408",
+    "url": "./static/css/main.f9449b38.chunk.css"
+  },
+  {
+    "revision": "6cd48ac8e19aa69e7517",
+    "url": "./static/js/2.378c97f5.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "./static/js/2.378c97f5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0121c617e01b805d5408",
+    "url": "./static/js/main.239fabc3.chunk.js"
+  },
+  {
+    "revision": "c776b87109dc4c8c209c",
+    "url": "./static/js/runtime-main.62e6bced.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/logo.75509f2e.png"
+  }
+]);
